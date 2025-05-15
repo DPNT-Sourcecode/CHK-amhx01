0586957be45468bc9f37b6277215f3f14ddfa1b2
sbx_user1051 <sbx_user1051@169.254.80.25>
2025-05-15 13:41:36 +0000

    Thu May 15 13:41:36 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,7 @@ class CheckoutSolution:
     # skus = unicode string
     def checkout(self, skus):
         try:
-            sku_count_dict = {
+            sku_special_items_count_dict = {
                 'A': 0,
                 'B': 0,
                 'E': 0,
@@ -14,14 +14,14 @@ class CheckoutSolution:
             for sku in skus:
                 if sku in standard_items:
                     total_value += standard_items[sku]
-                else:
-                    sku_count_dict[sku] +=1
+                if sku in sku_special_items_count_dict:
+                    sku_special_items_count_dict[sku] +=1
         except Exception as e:
             return e
 
-        sku_count_dict = self.edit_dict_for_gof_deal(sku_count_dict['E'], 2, 'B', sku_count_dict)
-        total_value += self.get_value_for_special_offers(130, 3, 50, sku_count_dict['A'])
-        total_value+= self.get_value_for_special_offers(45, 2, 30, sku_count_dict['B'])
+        sku_special_items_count_dict = self.edit_dict_for_gof_deal(sku_special_items_count_dict['E'], 2, 'B', sku_special_items_count_dict)
+        total_value += self.get_value_for_special_offers(130, 3, 50, sku_special_items_count_dict['A'])
+        total_value+= self.get_value_for_special_offers(45, 2, 30, sku_special_items_count_dict['B'])
 
         return total_value
     
@@ -31,6 +31,8 @@ class CheckoutSolution:
 
     
     def get_value_for_special_offers(self, special_value: int, offer_amount: int, normal_value: int, amount: int) -> int:
+        if amount == 0:
+            return 0
         if amount < offer_amount:
             return amount * normal_value
         else:
@@ -40,4 +42,5 @@ class CheckoutSolution:
 
 
         
+
 
