3fa209d97f007efc662864ce294e1fc18c4a1d19
sbx_user1051 <sbx_user1051@169.254.80.25>
2025-05-15 13:12:03 +0000

    Thu May 15 13:12:03 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -2,12 +2,16 @@ from lib.solutions.CHK.checkout_solution import CheckoutSolution
 
 
 class TestCheckout():
+    def test_checkout_for_singular_item(self):
+        assert CheckoutSolution().checkout('A') == 50
+        
     def test_checkouts_with_just_special_prices(self):
-        assert CheckoutSolution().checkout(['A', 'A', 'A','B','B']) == 175
+        assert CheckoutSolution().checkout('AAABB') == 175
     
     def test_checkouts_with_just_regular_prices(self):
-        assert CheckoutSolution().checkout(['A','B', 'C', 'D']) == 115
+        assert CheckoutSolution().checkout('ABCD') == 115
 
     def test_checkouts_with_special_prices_and_extra(self):
-        assert CheckoutSolution().checkout(['A', 'A', 'A', 'A']) == 180
+        assert CheckoutSolution().checkout('AAAA') == 180
+
 
