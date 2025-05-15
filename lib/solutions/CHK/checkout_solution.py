e696b35cfaad39a3889662f85ae648bd598bc724
sbx_user1051 <sbx_user1051@169.254.80.25>
2025-05-15 13:03:31 +0000

    Thu May 15 13:03:31 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,13 +17,25 @@ class CheckoutSolution:
                 sku_count_dict[sku] +=1
         
         if sku_count_dict['A'] <3:
-            a_value = sku_count_dict['A'] * 50
+            total_value += sku_count_dict['A'] * 50
         else:
-            sku_count_dict['A'] % 3
+            a_offer_value = sku_count_dict['A'] // 3 * 130
+            a_normal_value = sku_count_dict['A'] % 3 * 50
+            total_value = total_value + a_normal_value + a_offer_value
+        
+        if sku_count_dict['B'] < 2:
+            total_value += sku_count_dict['B'] * 30
+        else:
+            b_offer_value = sku_count_dict['A'] // 3 * 45
+            b_normal_value = sku_count_dict['A'] % 2 * 30
+            total_value = total_value + b_normal_value + b_offer_value
+
+        return total_value
             
 
 
         
+
 
 
 
